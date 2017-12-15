879fbaf2f95b1e00c5ba5d8536d1f9cd42ed70f1
itsabhinaya <abhinaya@Abhinayas-MacBook-Pro.local>
2017-12-15 14:12:27 -0600

    new intro

--- m25/js/index.js
+++ m25/js/index.js
@@ -1,4 +1,4 @@
- $(function(){ 
+$(function(){ 
      var navMain = $(".navbar-collapse"); // avoid dependency on #id
      // "a:not([data-toggle])" - to avoid issues caused
      // when you have dropdown inside navbar
